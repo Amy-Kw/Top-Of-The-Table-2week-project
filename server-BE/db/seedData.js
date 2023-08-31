4bc00dac9777baaaf88830ee888121816c5be25d
Amy KWong <amykwong6@gmail.com>
2023-08-31 10:28:58 -0400

    FK added to seeedData

--- server-BE/db/seedData.js
+++ server-BE/db/seedData.js
@@ -1,83 +1,74 @@
 //Make array of objects based on scema design here
 
 //user - goes to rate and info
-const user =[
-  {firstname: 'Boi', lastname: 'Pog', username: 'boardgame4life'},
-  {firstname: 'Jane', lastname: 'Floss', username: 'cards4thewin'},
-  {firstname: 'Chris', lastname: 'Ko', username: 'ggtabletop'}
+const users =[
+  {firstname: 'Boi', lastname: 'Pog', username: 'boardgame4life', password: 'pass123'},
+  {firstname: 'Jane', lastname: 'Floss', username: 'cards4thewin', password: 'account700'},
+  {firstname: 'Chris', lastname: 'Ko', username: 'ggtabletop', password: 'word456'}
 ]
 
 //info - goes to rate and list
 const info =[
-    {gametitle: 'Betrayal at House on the Hill', theme: 'fantasy', yearthatitcameout: '2004', expansion: true},
-    {gametitle: 'Wingspan', theme: 'birdwatching', yearthatitcameout: '2019', expansion: true},
-    {gametitle: 'steam up', theme: 'food', yearthatitcameout: '2023', expansion: false},
-    {gametitle: 'Splendor', theme: 'chip-collecting', yearthatitcameout: '2014', expansion: true},
-    {gametitle: 'project L', theme: 'puzzle', yearthatitcameout: '2020', expansion: true},
-    {gametitle: 'cartographers', theme: 'drawing', yearthatitcameout: '2019', expansion: true},
+    {gametitle: 'Betrayal at House on the Hill', theme: 'fantasy', year: '2004', expansions: true, ratelineId: 12},
+    {gametitle: 'Wingspan', theme: 'birdwatching', year: '2019', expansions: true, ratelineId: 7},
+    {gametitle: 'steam up', theme: 'food', year: '2023', expansions: false, ratelineId: 13},
+    {gametitle: 'Splendor', theme: 'chip-collecting', year: '2014', expansions: true, ratelineId: 2},
+    {gametitle: 'project L', theme: 'puzzle', year: '2020', expansions: true, ratelineId: 9},
+    {gametitle: 'cartographers', theme: 'drawing', year: '2019', expansions: true, ratelineId: 2},
   ]
 
-//rate - goes to user
+//rate - goes to users
 const rate =[
-    {recommended: true, setupcomplexities : '1', levelofdifficulties: '1'},
-    {recommended: true, setupcomplexities : '1', levelofdifficulties: '2'},
-    {recommended: true, setupcomplexities : '1', levelofdifficulties: '3'},
-    {recommended: true, setupcomplexities : '1', levelofdifficulties: '4'},
-    {recommended: true, setupcomplexities : '1', levelofdifficulties: '5'},
-    
-    {recommended: true, setupcomplexities : '2', levelofdifficulties: '1'},
-    {recommended: true, setupcomplexities : '2', levelofdifficulties: '2'},
-    {recommended: true, setupcomplexities : '2', levelofdifficulties: '3'},
-    {recommended: true, setupcomplexities : '2', levelofdifficulties: '4'},
-    {recommended: true, setupcomplexities : '2', levelofdifficulties: '5'},
-    
-    {recommended: true, setupcomplexities : '3', levelofdifficulties: '1'},
-    {recommended: true, setupcomplexities : '3', levelofdifficulties: '2'},
-    {recommended: true, setupcomplexities : '3', levelofdifficulties: '3'},
-    {recommended: true, setupcomplexities : '3', levelofdifficulties: '4'},
-    {recommended: true, setupcomplexities : '3', levelofdifficulties: '5'},
-
-    {recommended: true, setupcomplexities : '4', levelofdifficulties: '1'},
-    {recommended: true, setupcomplexities : '4', levelofdifficulties: '2'},
-    {recommended: true, setupcomplexities : '4', levelofdifficulties: '3'},
-    {recommended: true, setupcomplexities : '4', levelofdifficulties: '4'},
-    {recommended: true, setupcomplexities : '4', levelofdifficulties: '5'},
-
-    {recommended: true, setupcomplexities : '5', levelofdifficulties: '1'},
-    {recommended: true, setupcomplexities : '5', levelofdifficulties: '2'},
-    {recommended: true, setupcomplexities : '5', levelofdifficulties: '3'},
-    {recommended: true, setupcomplexities : '5', levelofdifficulties: '4'},
-    {recommended: true, setupcomplexities : '5', levelofdifficulties: '5'},
-
-    {recommended: false, setupcomplexities : '1', levelofdifficulties: '1'},
-    {recommended: false, setupcomplexities : '1', levelofdifficulties: '2'},
-    {recommended: false, setupcomplexities : '1', levelofdifficulties: '3'},
-    {recommended: false, setupcomplexities : '1', levelofdifficulties: '4'},
-    {recommended: false, setupcomplexities : '1', levelofdifficulties: '5'},
-    
-    {recommended: false, setupcomplexities : '2', levelofdifficulties: '1'},
-    {recommended: false, setupcomplexities : '2', levelofdifficulties: '2'},
-    {recommended: false, setupcomplexities : '2', levelofdifficulties: '3'},
-    {recommended: false, setupcomplexities : '2', levelofdifficulties: '4'},
-    {recommended: false, setupcomplexities : '2', levelofdifficulties: '5'},
-    
-    {recommended: false, setupcomplexities : '3', levelofdifficulties: '1'},
-    {recommended: false, setupcomplexities : '3', levelofdifficulties: '2'},
-    {recommended: false, setupcomplexities : '3', levelofdifficulties: '3'},
-    {recommended: false, setupcomplexities : '3', levelofdifficulties: '4'},
-    {recommended: false, setupcomplexities : '3', levelofdifficulties: '5'},
-
-    {recommended: false, setupcomplexities : '4', levelofdifficulties: '1'},
-    {recommended: false, setupcomplexities : '4', levelofdifficulties: '2'},
-    {recommended: false, setupcomplexities : '4', levelofdifficulties: '3'},
-    {recommended: false, setupcomplexities : '4', levelofdifficulties: '4'},
-    {recommended: false, setupcomplexities : '4', levelofdifficulties: '5'},
-
-    {recommended: false, setupcomplexities : '5', levelofdifficulties: '1'},
-    {recommended: false, setupcomplexities : '5', levelofdifficulties: '2'},
-    {recommended: false, setupcomplexities : '5', levelofdifficulties: '3'},
-    {recommended: false, setupcomplexities : '5', levelofdifficulties: '4'},
-    {recommended: false, setupcomplexities : '5', levelofdifficulties: '5'},
+    {recommended: true, setupcomplexities : '1', lvlofdifficulties: '1'},
+    {recommended: true, setupcomplexities : '1', lvlofdifficulties: '2'},
+    {recommended: true, setupcomplexities : '1', lvlofdifficulties: '3'},
+    {recommended: true, setupcomplexities : '1', lvlofdifficulties: '4'},
+    {recommended: true, setupcomplexities : '1', lvlofdifficulties: '5'}, /////////
+    {recommended: true, setupcomplexities : '2', lvlofdifficulties: '1'},
+    {recommended: true, setupcomplexities : '2', lvlofdifficulties: '2'},
+    {recommended: true, setupcomplexities : '2', lvlofdifficulties: '3'},
+    {recommended: true, setupcomplexities : '2', lvlofdifficulties: '4'},
+    {recommended: true, setupcomplexities : '2', lvlofdifficulties: '5'},/////////
+    {recommended: true, setupcomplexities : '3', lvlofdifficulties: '1'},
+    {recommended: true, setupcomplexities : '3', lvlofdifficulties: '2'},
+    {recommended: true, setupcomplexities : '3', lvlofdifficulties: '3'},
+    {recommended: true, setupcomplexities : '3', lvlofdifficulties: '4'},
+    {recommended: true, setupcomplexities : '3', lvlofdifficulties: '5'},////////
+    {recommended: true, setupcomplexities : '4', lvlofdifficulties: '1'},
+    {recommended: true, setupcomplexities : '4', lvlofdifficulties: '2'},
+    {recommended: true, setupcomplexities : '4', lvlofdifficulties: '3'},
+    {recommended: true, setupcomplexities : '4', lvlofdifficulties: '4'},
+    {recommended: true, setupcomplexities : '4', lvlofdifficulties: '5'},///////
+    {recommended: true, setupcomplexities : '5', lvlofdifficulties: '1'},
+    {recommended: true, setupcomplexities : '5', lvlofdifficulties: '2'},
+    {recommended: true, setupcomplexities : '5', lvlofdifficulties: '3'},
+    {recommended: true, setupcomplexities : '5', lvlofdifficulties: '4'},
+    {recommended: true, setupcomplexities : '5', lvlofdifficulties: '5'},///////
+    {recommended: false, setupcomplexities : '1', lvlofdifficulties: '1'},
+    {recommended: false, setupcomplexities : '1', lvlofdifficulties: '2'},
+    {recommended: false, setupcomplexities : '1', lvlofdifficulties: '3'},
+    {recommended: false, setupcomplexities : '1', lvlofdifficulties: '4'},
+    {recommended: false, setupcomplexities : '1', lvlofdifficulties: '5'},//////
+    {recommended: false, setupcomplexities : '2', lvlofdifficulties: '1'},
+    {recommended: false, setupcomplexities : '2', lvlofdifficulties: '2'},
+    {recommended: false, setupcomplexities : '2', lvlofdifficulties: '3'},
+    {recommended: false, setupcomplexities : '2', lvlofdifficulties: '4'},
+    {recommended: false, setupcomplexities : '2', lvlofdifficulties: '5'},//////
+    {recommended: false, setupcomplexities : '3', lvlofdifficulties: '1'},
+    {recommended: false, setupcomplexities : '3', lvlofdifficulties: '2'},
+    {recommended: false, setupcomplexities : '3', lvlofdifficulties: '3'},
+    {recommended: false, setupcomplexities : '3', lvlofdifficulties: '4'},
+    {recommended: false, setupcomplexities : '3', lvlofdifficulties: '5'},//////
+    {recommended: false, setupcomplexities : '4', lvlofdifficulties: '1'},
+    {recommended: false, setupcomplexities : '4', lvlofdifficulties: '2'},
+    {recommended: false, setupcomplexities : '4', lvlofdifficulties: '3'},
+    {recommended: false, setupcomplexities : '4', lvlofdifficulties: '4'},
+    {recommended: false, setupcomplexities : '4', lvlofdifficulties: '5'},//////
+    {recommended: false, setupcomplexities : '5', lvlofdifficulties: '1'},
+    {recommended: false, setupcomplexities : '5', lvlofdifficulties: '2'},
+    {recommended: false, setupcomplexities : '5', lvlofdifficulties: '3'},
+    {recommended: false, setupcomplexities : '5', lvlofdifficulties: '4'},
+    {recommended: false, setupcomplexities : '5', lvlofdifficulties: '5'},//////
 ]
 
 //list
@@ -93,4 +84,4 @@ const list =[
 
 ]
 
-module.exports = { user, info, rate, list }
+module.exports = { users, info, rate, list }
